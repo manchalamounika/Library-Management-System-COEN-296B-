cf60fd5feee1052aa44fb80f15861863a1fbcd4d
RAPatel24 <41607110+RAPatel24@users.noreply.github.com>
2018-09-10 23:01:47 -0700

    Update AddNewBookForm.js
    drop down list to libraries

--- src/containers/Book/AddNewBookForm.js
+++ src/containers/Book/AddNewBookForm.js
@@ -8,6 +8,10 @@ import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import axios from 'axios';
@@ -30,7 +34,11 @@ const styles = theme => ({
   },
   button:{
     margin: theme.spacing.unit,
-  }
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
 });
 
 
@@ -47,7 +55,20 @@ state = {
     formErrorMessage:'',
     open: false,
 }
-
+componentDidMount() {
+    ValidatorForm.addValidationRule('isString', (value) => {
+        if (value.match(/^[0-9]/)) {
+            return false;
+        }
+        return true;
+    });
+    ValidatorForm.addValidationRule('isNumber', (value) => {
+        if (value.match(/^[a-zA-Z]+$/)) {
+            return false;
+        }
+        return true;
+    });
+}
 handleChange = prop => event => {
     this.setState({ [prop]: event.target.value });
 }
@@ -103,55 +124,67 @@ closeHandler=() =>{
                 id="margin-normal"
                 placeholder="book title" type="text" name="title" value={this.state.title}
                 onChange={this.handleChange('title')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                validators={['isString','required']}
+                errorMessages={['alphabets only','this field is required']} />
                 <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Book Id"
                 id="margin-normal"
                 placeholder="Book Id" type="text" name="bookId" value={this.state.bookId}
                 onChange={this.handleChange('bookId')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                validators={['isNumber','required']}
+                errorMessages={['numbers only','this field is required']} />
                 <TextValidator                  
                 className={classNames(classes.margin, classes.textField)}
                 label="Author First Name"
                 id="margin-normal"                
                 placeholder="First Name" type="text" name="firstName" value={this.state.firstName}
                 onChange={this.handleChange('firstName')} 
-                validators={['required']}
-                errorMessages={['this field is required']}
+                validators={['isString','required']}
+                errorMessages={['alphabets only','this field is required']}
                 />
                 <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Author Middle Name"
                 id="margin-normal"
                 placeholder="Middle Name" type="text" name="middleName" value={this.state.middleName}                
-                onChange={this.handleChange('middleName')} />
+                onChange={this.handleChange('middleName')} 
+                validators={['isString','required']}
+                errorMessages={['alphabets only','this field is required']}/>
             <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label=" Author Last Name"
                 id="margin-normal"
                 placeholder="Last Name" type="text" name="lastName" value={this.state.lastName}
                 onChange={this.handleChange('lastName')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
-                <TextValidator
-                className={classNames(classes.margin, classes.textField)}
-                label="Library"
-                id="margin-normal"
-                placeholder="Choose from Library" type="text" name="libraryName" value={this.state.libraryName}
+                validators={['isString','required']}
+                errorMessages={['alphabets only','this field is required']}/>
+                <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="library-name">Library Name</InputLabel>
+            <Select
+                value={this.state.libraryName}
                 onChange={this.handleChange('libraryName')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                inputProps={{
+                name: 'libraryName',
+                id: 'library-name',
+            }}
+            >
+            <MenuItem value="SCU">
+                </MenuItem>
+                <MenuItem value="SCU">SCU</MenuItem>
+                <MenuItem value="Central Park">Central Park</MenuItem>
+                <MenuItem value="Lib1">Lib1</MenuItem>
+                <MenuItem value="Lib2">Lib2</MenuItem>
+            </Select>
+            </FormControl>
                 <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Barcode"
                 id="margin-normal"
                 placeholder="Barcode" type="text" name="barCode" value={this.state.barCode}
                 onChange={this.handleChange('barCode')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                validators={['isNumber','required']}
+                errorMessages={['numbers only','this field is required']}/>
                 <Button
                 className={classes.button}
                 size="small"
