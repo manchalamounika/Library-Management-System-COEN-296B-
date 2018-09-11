5310846b006e986e49b792f42c4ca04ce2e695f0
RAPatel24 <41607110+RAPatel24@users.noreply.github.com>
2018-09-10 23:05:04 -0700

    Update AddNewReaderForm.js
    dropdown list and validations

--- src/containers/Readers/AddNewReaderForm.js
+++ src/containers/Readers/AddNewReaderForm.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -30,7 +30,11 @@ const styles = theme => ({
   },
   button:{
     margin: theme.spacing.unit,
-  }
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
 });
 
 
@@ -44,6 +48,20 @@ state = {
     showFormValidation:false,
     formErrorMessage:'',
     open: false,
+}
+componentDidMount() {
+    ValidatorForm.addValidationRule('isString', (value) => {
+        if (value.match(/^[0-9]/)) {
+            return false;
+        }
+        return true;
+    });
+    ValidatorForm.addValidationRule('isNumber', (value) => {
+        if (value.match(/^[a-zA-Z]+$/)) {
+            return false;
+        }
+        return true;
+    });
 }
 
 handleChange = prop => event => {
@@ -103,39 +121,51 @@ render() {
                 id="margin-normal"                
                 placeholder="First Name" type="text" name="firstName" value={this.state.firstName}
                 onChange={this.handleChange('firstName')} 
-                validators={['required']}
-                errorMessages={['this field is required']}
+                validators={['isString','required']}
+                errorMessages={['Only Alphabets','this field is required']}
                 />
             <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Middle Name"
                 id="margin-normal"
                 placeholder="Middle Name" type="text" name="middleName" value={this.state.middleName}                
-                onChange={this.handleChange('middleName')} />
+                onChange={this.handleChange('middleName')}
+                validators={['isString','required']}
+                errorMessages={['this field is required']} />
             <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Last Name"
                 id="margin-normal"
                 placeholder="Last Name" type="text" name="lastName" value={this.state.lastName}
                 onChange={this.handleChange('lastName')}
-                validators={['required']}
+                validators={['isString','required']}
                 errorMessages={['this field is required']} />
-                <TextValidator
-                className={classNames(classes.margin, classes.textField)}
-                label="Library"
-                id="margin-normal"
-                placeholder="Choose from Library" type="text" name="libraryName" value={this.state.libraryName}
+                <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="library-name">Library Name</InputLabel>
+            <Select
+                value={this.state.libraryName}
                 onChange={this.handleChange('libraryName')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                inputProps={{
+                name: 'libraryName',
+                id: 'library-name',
+            }}
+            >
+            <MenuItem value="SCU">
+                </MenuItem>
+                <MenuItem value="SCU">SCU</MenuItem>
+                <MenuItem value="Central Park">Central Park</MenuItem>
+                <MenuItem value="Lib1">Lib1</MenuItem>
+                <MenuItem value="Lib2">Lib2</MenuItem>
+            </Select>
+            </FormControl>
                 <TextValidator
                 className={classNames(classes.margin, classes.textField)}
                 label="Barcode"
                 id="margin-normal"
                 placeholder="Barcode" type="text" name="barCode" value={this.state.barCode}
                 onChange={this.handleChange('barCode')}
-                validators={['required']}
-                errorMessages={['this field is required']} />
+                validators={['isNumber','required']}
+                errorMessages={['Numbers only','this field is required']} />
                 <Button
                 className={classes.button}
                 size="small"
